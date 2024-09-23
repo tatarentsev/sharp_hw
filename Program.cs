ecc432aa72a6469d157cfaecccddd980f3fe9c40
ilyaTATARENTSEV <kaxaaapbg@mail.ru>
2024-09-23 13:23:48 +0300

    Home Work is Finished

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿// // Задача №2
+
+
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine($"Максимальное число из чисел {num1} и {num2} является {num1}");
+// }
+
+// else
+// {
+//     Console.WriteLine($"Максимальное число из чисел {num1} и {num2} является {num2}");
+// }
+
+
+
+// Задача №4
+
+
+
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// int max_num;
+
+// if (num1 < num2)
+// {
+//     max_num = num2;
+// }
+
+// else 
+// {
+//     max_num = num1;
+// }
+
+// if (max_num < num3)
+// {
+//     max_num = num3;
+// }
+
+// Console.WriteLine($"Наибольшее число из чисел {num1}, {num2}, {num3} - это {max_num}");
+
+
+
+// Задача №6
+
+
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 == 0)
+// {
+//     Console.WriteLine($"Число {num} является четным!");
+// }
+
+// else 
+// {
+//     Console.WriteLine($"Число {num} является не четным :(");
+// }
+
+
+
+// Задача № 8
+
+
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int count = 2;
+
+// while (count < num + 1)
+// {
+//     if (count % 2 == 0)
+//     {
+//         Console.Write(count + ", ");
+//         count += 2;
+//     }
+
+//     else
+//     {
+//         count++; 
+//         Console.Write(count + ", ");
+//     }
+// }
+
+// Console.Write("\b\b");
+
+// Home Work Finished.
